622cb54437160d4b8c905cb7dff6221bdb310abe
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-17 12:03:00 +0200

    API to Heroku

--- UD25-API01/src/main/java/com/example/api_rest/dao/IArticuloDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.api_rest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.api_rest.dto.Articulo;
-
-public interface IArticuloDAO extends JpaRepository<Articulo, Long>{
-
-}
--- UD25-API01/src/main/java/com/example/api_rest/dao/IFabricanteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.api_rest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.api_rest.dto.Fabricante;
-
-public interface IFabricanteDAO extends JpaRepository<Fabricante, Long> {
-
-}
